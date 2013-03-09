553e153f3de9b3cfdd8639e7855dd233e926174b
madisonmay13 <madison.may@students.olin.edu>
2013-03-08 21:47:04 -0500

    Direct to /home

--- routes/lectures.js
+++ routes/lectures.js
@@ -11,7 +11,7 @@ Array.prototype.sortByProp = function(p) {
 };
 
 exports.login = function(req, res) {
-    res.redirect('http://olinapps.com/external?callback=http://olin-lectures.herokuapp.com/')
+    res.redirect('http://olinapps.com/external?callback=http://olin-lectures.herokuapp.com/home')
 }
 
 exports.list = function(req, res) {
